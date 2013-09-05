d7a332fd1eddcf6407e5438147bfbbf38f6fc59d
futur24 <velerofont@agnostosx-laptop.lan>
2013-09-05 13:49:04 +0300

    getDogById function in dog model changed

--- app/Model/Dog.php
+++ app/Model/Dog.php
@@ -5,7 +5,7 @@ class Dog extends AppModel {
      
     
     function getDogById($dogId){
-        $sql = "SELECT d.id, d.name, db.name, d.age, d.gender, d.weight, p.path, dl.id";
+        $sql = "SELECT d.id, d.name, db.name, d.age, d.gender, d.weight, p.path, dl.id, count(dl.id) as likes";
         $sql .= " FROM dogs d";
         $sql .= " LEFT OUTER JOIN dog_breeds db on (d.breed_id = db.id)";
         $sql .= " LEFT OUTER JOIN photos p on (d.photo_id = p.id)";
@@ -14,22 +14,20 @@ class Dog extends AppModel {
         $rs = $this->query($sql);
         $obj = array();
         
-        if(is_array($rs)){
-            foreach($rs as $i => $values){
-                $obj['Dog']['id'] = $rs[$i]['d']['id'];
-		$obj['Dog']['name'] = $name = $rs[$i]['d']['name'];
-		$obj['Dog']['dog_breed'] = $rs[$i]['db']['name'];
-                $obj['Dog']['age'] = $rs[$i]['d']['age'];
-                $obj['Dog']['gender'] = $rs[$i]['d']['gender'];
-                $obj['Dog']['weight'] = $rs[$i]['d']['weight'];
-                $obj['Dog']['photo'] = $rs[$i]['p']['path'];
-                $obj['Dog']['liked'] = $rs[$i]['dl']['id'];
-                
-		$data[] = $obj;
-            }
-        }
         
-        return $data;
+        $obj['id'] = $rs[0]['d']['id'];
+        $obj['name'] = $name = $rs[0]['d']['name'];
+        $obj['dog_breed'] = $rs[0]['db']['name'];
+        $obj['age'] = $rs[0]['d']['age'];
+        $obj['gender'] = $rs[0]['d']['gender'];
+        $obj['weight'] = $rs[0]['d']['weight'];
+        $obj['photo'] = $rs[0]['p']['path'];
+        $obj['liked'] = $rs[0]['dl']['id'];
+        $obj['likes'] = $rs[0][0]['likes'];
+                
+		
+        
+        return $obj;
     }
 }
 
