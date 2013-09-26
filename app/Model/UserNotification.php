cc1dbd74974f2a685bfec9f2e828e8c8e252379a
jkritikos <jkritikos@gmail.com>
2013-09-26 17:53:28 +0300

    getting the time as timestmap for notifications

--- app/Model/UserNotification.php
+++ app/Model/UserNotification.php
@@ -5,7 +5,7 @@ class UserNotification extends AppModel {
     
     //Returns the unread notifications for this user
     function getUnreadNotifications($user_id){
-        $sql = "select un.id, un.user_from, un.type_id, un.activity_id, p.thumb, u.name, un.created from user_notifications un ";
+        $sql = "select un.id, un.user_from, un.type_id, un.activity_id, p.thumb, u.name, UNIX_TIMESTAMP(un.created) created from user_notifications un ";
         $sql .= "inner join users u on (u.id=un.user_from) inner join photos p on (p.id=u.photo_id) where un.user_id=$user_id and un.read=0 order by id desc";
         $rs = $this->query($sql);
         $data = array();
@@ -13,7 +13,7 @@ class UserNotification extends AppModel {
 	if(is_array($rs)){
             foreach($rs as $i => $values){
                 $obj['id'] = $rs[$i]['un']['id'];
-                $obj['created'] = $rs[$i]['un']['created'];
+                $obj['created'] = $rs[$i][0]['created'] * 1000;
                 $obj['name'] = $rs[$i]['u']['name'];
                 $obj['user_from'] = $rs[$i]['un']['user_from'];
                 $obj['type_id'] = $rs[$i]['un']['type_id'];
